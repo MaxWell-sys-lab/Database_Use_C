d56ad54705fd4df7b2a24ba6b2a8d21b4c9bd353
MaxWell <718848752@qq.com>
2024-09-01 19:28:24 +0800

    refactor: 添加存储行、表、数据序列化及插入、查询操作

--- res/main.c
+++ res/main.c
@@ -2,6 +2,43 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
+#define COLUMN_USERNAME_SIZE 32     // 用户名的大小
+#define COLUMN_EMAIL_SIZE 255       // 邮箱的大小
+
+/**
+ * 存储行的结构体
+ */
+typedef struct
+{
+    int id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)   // 获取结构体中某个属性的大小
+const uint32_t ID_SIZE = size_of_attribute(Row, id);                // id的大小
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);    // username的大小
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);          // email的大小
+const uint32_t ID_OFFSET = 0;                                       // id的偏移量
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;               // username的偏移量
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;      // email的偏移量
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;     // 行的大小
+
+const uint32_t PAGE_SIZE = 4096;                                        // 页的大小
+#define TABLE_MAX_PAGES 100                                             // 最大页数
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / size_of_attribute(Row, id);  // 每页的行数
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;        // 最大行数
+
+/**
+ * 表
+ */
+typedef struct
+{
+    uint32_t num_rows;              // 行数
+    void *pages[TABLE_MAX_PAGES];   // 页
+} Table;
 
 /**
  * 输入缓冲区
@@ -28,6 +65,7 @@ typedef enum
 typedef enum
 {
     PREPARE_SUCCESS,                // 准备成功
+    PREPARE_SYNTAX_ERROR,           // 语法错误
     PREPARE_UNRECOGNIZED_STATEMENT  // 未识别的语句
 } PrepareResult;
 
@@ -46,10 +84,65 @@ typedef enum
 typedef struct
 {
     StatementType type;             // 语句类型
+    Row row_to_insert;              // 插入的行，只有在语句类型为STATEMENT_INSERT时有效
 } Statement;
 
 /**
+ * 执行结果
+ */
+typedef enum
+{
+    EXECUTE_SUCCESS,                // 执行成功
+    EXECUTE_TABLE_FULL              // 表已满
+} ExecuteResult;
+
+/**
+ * 序列化行
+ * @param source 源行
+ * @param destination 目标地址
+ */
+void serialize_row(Row *source, void *destination)
+{
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+/**
+ * 反序列化行
+ * @param source 源地址
+ * @param destination 目标行
+ */
+void deserialize_row(void *source, Row *destination)
+{
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+/**
+ * 获取某一行地址
+ * @param table 表
+ * @param row_num 行号
+ * @return 该行地址
+ */
+void *row_slot(Table *table, uint32_t row_num)  
+{
+    uint32_t page_num = row_num / ROWS_PER_PAGE;    // 页号
+    void *page = table->pages[page_num];            // 页面起始地址
+    if(page == NULL)
+    {
+        page = table->pages[page_num] = malloc(PAGE_SIZE);  // 分配页的内存空间
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;                  // 行偏移量
+    uint32_t byte_offset = row_offset * ROW_SIZE;                   // 字节偏移量
+    return page + byte_offset;
+}
+
+/**
  * 语句处理
+ * @param input_buffer 输入缓冲区
+ * @return 元命令执行结果
  */
 MetaCommandResult do_meta_command(InputBuffer *input_buffer)
 {
@@ -74,6 +167,11 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     if(strncmp(input_buffer->buffer, "insert", 6) == 0) // 因为insert语句会包含其他字符，所以只需要比较前6个字符
     {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+        if(args_assigned < 3)
+        {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if(strcmp(input_buffer->buffer, "select") == 0)
@@ -86,20 +184,86 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 }
 
 /**
+ * 执行插入语句
+ * @param statement 语句
+ * @param table 表
+ * @return 执行结果
+ */
+ExecuteResult execute_insert(Statement *statement, Table *table)
+{
+    if(table->num_rows >= TABLE_MAX_ROWS)
+    {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    return EXECUTE_SUCCESS;
+}
+
+/**
+ * 执行查询语句
+ * @param statement 语句
+ * @param table 表
+ * @return 执行结果
+ */
+ExecuteResult execute_select(Statement *statement, Table *table)
+{
+    Row row;
+    for(uint32_t i = 0; i < table->num_rows; i++)
+    {
+        deserialize_row(row_slot(table, i), &row);
+        printf("%d %s %s\n", row.id, row.username, row.email);
+    }
+
+    return EXECUTE_SUCCESS;
+}
+
+/**
  * 执行语句
  * @param statement 语句
+ * @param table 表
+ * @return 执行结果
  */
-void execute_statement(Statement *statement)
+ExecuteResult execute_statement(Statement *statement, Table *table)
 {
     switch(statement->type)
     {
         case STATEMENT_INSERT:
-            printf("This is where we would do an insert.\n");
-            break;
+            return execute_insert(statement, table);
         case STATEMENT_SELECT:
-            printf("This is where we would do a select.\n");
-            break;
+            return execute_select(statement, table);
     }
+}
+
+/**
+ * 创建表
+ * @return 表指针
+ */
+Table *new_table()
+{
+    Table *table = (Table *)malloc(sizeof(Table));
+    table->num_rows = 0;
+    for(uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+    {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+/**
+ * 释放表
+ * @param table 表
+ */
+void free_table(Table *table)
+{
+    for(int i = 0; table->pages[i]; i++)
+    {
+        free(table->pages[i]);
+    }
+    free(table);
 }
 
 /**
@@ -159,6 +323,7 @@ void close_input_buffer(InputBuffer *input_buffer)
  */
 int main(int argc, char *argv[])
 {
+    Table *table = new_table();    // 创建表
     InputBuffer *input_buffer = new_input_buffer();    // 创建输入缓冲区
 
     while(true)
@@ -183,12 +348,22 @@ int main(int argc, char *argv[])
         {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error. Could not parse statement.\n");    // 打印错误信息
+                continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);    // 打印错误信息
                 continue;
         }
 
-        execute_statement(&statement);
-        printf("Executed.\n");
+        switch(execute_statement(&statement, table))
+        {
+            case (EXECUTE_SUCCESS):
+                printf("Executed.\n");    // 打印执行成功信息
+                break;
+            case (EXECUTE_TABLE_FULL):
+                printf("Error: Table full.\n");    // 打印错误信息
+                break;
+        }
     }
 }
